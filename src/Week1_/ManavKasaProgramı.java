17450a3a6f1bfd00dfc2a7528bdba1a486cf761f
ebrucihan <ebrchnnn123@gmail.com>
2024-04-24 00:11:26 +0300

    Manav Kasa Programı

--- src/Week1_/ManavKasaProgramı.java
+++ src/Week1_/ManavKasaProgramı.java
@@ -2,12 +2,14 @@ package Week1_;
 import java.util.Scanner;
 public class ManavKasaProgramı {
     public static void main(String[] args) {
+        // Değerlerin fiyatlarını tanımladığımız bölüm
         double armutFiyat = 2.14;
         double elmaFiyat = 3.67;
         double domatesFiyat = 1.11;
         double muzFiyat= 0.95;
         double patlıcanFiyat= 5.00;
 
+        //Kullanıcıdan değer aldığımız bölüm
         Scanner input = new Scanner(System.in);
         System.out.println("Armut Kaç Kilo ?");
         double armutKg = input.nextDouble();
@@ -20,10 +22,9 @@ public class ManavKasaProgramı {
         System.out.println("Patlıcan kaç kilo?");
         double patlıcanKg = input.nextDouble();
 
+       //Toplam tutarın hesaplanması
         double ToplamTutar = (armutFiyat*armutKg)+ (elmaFiyat*elmaKg)+(domatesFiyat*domatesKg)+(muzFiyat*muzKg)+(patlıcanFiyat*patlıcanKg);
 
         System.out.println("Toplam Tutar:" + ToplamTutar + "TL");
-
-
     }
 }
