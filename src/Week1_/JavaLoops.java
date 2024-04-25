8abe668191c63b7c27fa8c67e9520011182a5bcd
ebrucihan <ebrchnnn123@gmail.com>
2024-04-25 17:31:06 +0300

    Commit eklendi.

--- src/Week1_/JavaLoops.java
+++ src/Week1_/JavaLoops.java
@@ -2,15 +2,30 @@ package Week1_;
 import java.util.Scanner;
 public class JavaLoops {
     public static void main(String[] args) {
-        int a ;
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Sayı Giriniz: ");
-        a = inp.nextInt();
+        // Kullanıcıdan bir sayı almak için Scanner oluşturulur.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int n = scanner.nextInt();
 
-        for (int i= 0 ; i <= a ; i++) {
-            if(i%4==0 && i%3==0) {
-                System.out.println(i);
+        // Toplam ve sayacı başlatılır.
+        int total = 0;
+        int count = 0;
+
+
+        for (int i = 0; i <= n; i++) {
+            // Eğer sayı 3 ve 4'e tam bölünüyorsa:
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                count++;
             }
+        }
+
+        if (count > 0) {
+            // Ortalama hesaplanır.
+            double average = (double) total / count;
+            System.out.println("0'dan " + n + "'e kadar olan sayıların ortalaması: " + average);
+        } else {
+            System.out.println("0'dan " + n + "'e kadar hiçbir sayı 3 ve 4'e tam bölünmüyor.");
         }
     }
 }
